6deceeece4f219357dc0eb522d9d5704692de29c
J-Stiehl <salsajackie@yahoo.com>
2023-08-04 09:35:16 -0500

    get and post routes working

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -21,32 +21,6 @@ router.get("/", (req, res) => {
   });
 });
 
-// * `GET /api/notes` should read the `db.json` file
-//and return all saved notes as JSON.
-// router.get("/api/notes", (req, res) => res.json(dbData));
-
-// * `POST /api/notes` should receive a new note to save on
-//the request body, add it to the `db.json` file, and then return
-//the new note to the client. You'll need to find a way to give
-//each note a unique id when it's saved (look into npm packages
-//that could do this for you).
-
-//my note: I did "npm install uuid" on 8-2-23 for unique ids
-
-//prepare a response object to send back to the client
-//   let response;
-//   //from lesson 15 server.js file
-//   if (req.body && req.body.product) {
-//     response = {
-//       status: "Success",
-//       data: req.body,
-//     };
-//     res.status(201).json(response);
-//   } else {
-//     res.status(400).json("Must contain a note title");
-//   }
-//   console.log(req.body);
-
 //route for creating a new note
 // 1. read the existing file
 //  2. make the file contents into a javascript array/ object
@@ -54,42 +28,31 @@ router.get("/", (req, res) => {
 // 4. Re-write the json file with the new data
 // 5. Send a response back to the browser
 
-//OR:(from 19 Data Persistence)
 router.post("/", (req, res) => {
   const { title, text } = req.body;
   if (title && text) {
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
     console.log(newNote);
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
       const parsed = JSON.parse(data);
       parsed.push(newNote);
+      fs.writeFile(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify(parsed),
+        (err) => {
+          err
+            ? console.error(err)
+            : console.log(
+                `New note ${newNote.product} has been added to JSON file`
+              );
+        }
+      );
+      res.status(200).json(parsed);
     });
-    fs.appendFile(
-      path.join(__dirname, "../db/db.json"),
-      `${newNote}.json`,
-      (err) => {
-        err
-          ? console.error(err)
-          : console.log(
-              `New note ${newNote.product} has been added to JSON file`
-            );
-      }
-    );
-    res.status(200).json(parsed);
-
-    //     //prepare a response object to send back to the client
-    //       //from lesson 15 server.js file
-    let response = {
-      status: "Success",
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
   } else {
     res.status(400).json("Must contain a note title");
   }
